fff6d3dabfe6a2a01631bdeeed2263e7adb545d3
Arturo Silvelo <asilvelo@trynewroads.com>
2025-07-29 09:40:11 +0200

    feat: refactor app module and main bootstrap to conditionally use LoggingInterceptor based on debugRequest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { envValidationSchema } from './config/env.validation';
 import { DatabaseModule } from './database/database.module';
 import { TasksModule } from './tasks/tasks.module';
@@ -19,11 +17,5 @@ import { TasksModule } from './tasks/tasks.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: LoggingInterceptor,
-    },
-  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { ApiConfigService } from './config/api-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
   app.setGlobalPrefix('api');
+
+  const apiConfigService = app.get(ApiConfigService);
+
+  if (apiConfigService.debugRequest) {
+    app.useGlobalInterceptors(new LoggingInterceptor(apiConfigService));
+  }
 
   const config = new DocumentBuilder()
     .setTitle('TODO API')
@@ -17,6 +25,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(apiConfigService.port);
 }
 void bootstrap();
